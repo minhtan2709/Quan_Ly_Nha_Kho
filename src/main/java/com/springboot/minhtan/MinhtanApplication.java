cc43dc204290826112101ade3c751f8c273a3bad
minhtan2709 <tantran2709vn@gmail.com>
2025-03-13 22:43:22 +0700

    Create project

---
+++ src/main/java/com/springboot/minhtan/MinhtanApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.minhtan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhtanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhtanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/minhtan/MinhtanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.minhtan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhtanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
